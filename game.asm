9193e8b9545adbd52d43f44341f375d466814010
Benjamin Berger <benjamin.berger@sandbox.game>
2025-03-04 15:38:41 +0100

    Working decision actions!

--- game.asm
+++ game.asm
@@ -9,11 +9,14 @@ section .data
     max_input_tries db 3
 
     ; Texts
-    err_invalid_input: db "You need to enter a value between 0 and 9!", 10, 0
+    err_invalid_input: db "You need to enter a value between 1 and 9!", 10, 0
     err_invalid_input_l equ $ - err_invalid_input
 
     err_too_many_invalid_input: db "While thinking about your decision, the time passed by and you turned into a mummy.", 10, 0
     err_too_many_invalid_input_l equ $ - err_too_many_invalid_input
+
+    err_invalid_action: db "Impossible!", 10, 10, 0
+    err_invalid_action_l equ $ - err_invalid_action
 
     txt_game_over db 10, "***************************", 10, "******** GAME OVER ********", 10, "***************************", 10, 10, "Thank you for playing! Better luck next time!", 10, 0
     txt_game_over_l equ $ - txt_game_over
@@ -21,24 +24,39 @@ section .data
     txt_input_confirm db "Your decision is: ", 0
     txt_input_confirm_l equ $ - txt_input_confirm
 
-    ; Texts - Decisions
-    txt_dc_initial:
-        db  "You are in a dark room. What do you do?", 10, "   1) Turn on the light", 10, "   2) Exit the game. Too creepy here.", 10, 10, 0
-    txt_dc_initial_l equ $ - txt_dc_initial
-
-    txt_dc_flavor db "Is that a dream? Is that reality?", 0
-    txt_dc_flavor_l equ $ - txt_dc_flavor
+    txt_decision_taken db "---------------------------------------------------------------------------", 10, 0
+    txt_decision_taken_l equ $ - txt_decision_taken
 
     ; Decisions
     dc_initial:
         dq txt_dc_initial
         dd txt_dc_initial_l
         dq dc_flavor
+        dq dc_game_end
+        dq 0
+
+    txt_dc_initial:
+        db  "You are in a dark room. What do you do?", 10, "   1) Turn on the light", 10, "   2) Exit the game. Too creepy here.", 10, 10, 0
+    txt_dc_initial_l equ $ - txt_dc_initial
 
     dc_flavor:
         dq txt_dc_flavor
         dd txt_dc_flavor_l
-        dq 0x0
+        dq dc_initial
+        dq 0
+
+    txt_dc_flavor db "Is that a dream? Is that reality?", 10, "   1) Do something", 10, 10, 0
+    txt_dc_flavor_l equ $ - txt_dc_flavor
+
+    dc_game_end:
+        dq txt_dc_game_end
+        dd txt_dc_game_end_l
+        dq 0
+
+    txt_dc_game_end db "The dream is over! Head back to reality.", 10, 0
+    txt_dc_game_end_l equ $ - txt_dc_game_end
+
+    ;dc_area_0_turn_on_light:
 
     hConsoleOut dq 0
     hConsoleIn dq 0
@@ -78,24 +96,75 @@ main_loop:
     mov rcx, [rcx]
     call WriteText
 
+    ; Get the action count of the decision
+    mov rcx, [current_decision]
+    call GetActionCount
+
+    cmp rax, 0x0
+    je EndGame
+
+    push rax                        ; push action count on stack
+
     ; Read the decision from the input
     mov rcx, txt_input_confirm
     mov rdx, txt_input_confirm_l
     call WriteText
 
-    call ReadDigit
-    
+    call ReadDigit                  ; selected digit in rax
 
-    ; React on decision
-    
-    ; Exit
-    jmp EndGame
+    pop rdx
 
+    cmp rax, rdx
+    jae _invalid_action
 
+    mov rcx, [current_decision]
+    mov rdx, rax
+    call GetActionTarget
+    mov [current_decision], rax
+
+    mov rcx, txt_decision_taken
+    mov rdx, txt_decision_taken_l
+    call WriteText
+
+    jmp main_loop
+
+_invalid_action:
+    mov rcx, err_invalid_action
+    mov rdx, err_invalid_action_l
+    call WriteText
+    jmp main_loop
+
+GetActionTarget:
+    ; rcx => decision address
+    ; rdx => action index
+    ; rax = returns action target decision address
+    shl rdx, 3  ; multiply by 8 since an action address is 8 bytes
+    add rcx, 12
+    add rcx, rdx
+    mov rax, [rcx]
+    ret
+
+GetActionCount:
+    ; rcx => decision address
+    mov rax, 0
+    add rcx, 12     ; Jump to the first possible action
+
+_action_counter_loop:
+    mov rdx, [rcx]  ; Load the
+    cmp rdx, 0
+    je _action_return
+
+    inc rax
+    add rcx, 8
+    jmp _action_counter_loop
+
+_action_return
+    ret
 
 ReadDigit:
     ; Reads input from the console
     ; Converts it to a number and verifies that the value is between 0 and 9
+    ; Returns digit in rax
 
     mov r10, 0
 
@@ -117,7 +186,10 @@ _read_digit_loop:
     sub al, '0'
     cmp al, 9
     ja _invalid_digit_input
+    cmp al, 0
+    je _invalid_digit_input
 
+    sub rax, 1
     ret
 
 _invalid_digit_input:
@@ -143,16 +215,6 @@ _too_many_inputs:
 
     jmp EndGame
 
-EndGame:
-    ; Print game over text and ends the process
-    mov rcx, txt_game_over
-    mov rdx, txt_game_over_l
-    call WriteText
-
-    ; Exit
-    xor ecx, ecx
-    call ExitProcess
-
 WriteText:
     ; rcx - Pointer to message
     ; rdx - Message length
@@ -164,3 +226,13 @@ WriteText:
     call WriteConsoleA
     pop rax
     ret
+
+EndGame:
+    ; Print game over text and ends the process
+    mov rcx, txt_game_over
+    mov rdx, txt_game_over_l
+    call WriteText
+
+    ; Exit
+    xor ecx, ecx
+    call ExitProcess
